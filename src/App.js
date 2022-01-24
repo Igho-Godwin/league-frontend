cb793d197ce55336eb9b927d666017e925cb3f58
Avwerosuo Igho-Godwin <avwerosuoigho-godwin@Avwerosuos-MacBook-Pro.local>
2022-01-24 02:40:06 +0100

    changes to for package.json for production

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
 } from "react-router-dom";
 
+
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import NoMatch from "./components/NoMatch";
